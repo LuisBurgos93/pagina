4f6776afffaf8e1d6da4b44c49d80dfc08111e8f
LuisBurgos93 <burgos.luisfernando@gmail.com>
2018-09-24 15:52:45 -0500

    javascript con clase

--- javascript/script.js
+++ javascript/script.js
@@ -1,16 +1,42 @@
+class veriStrings{
+	constructor(nombre, telefono){
+		this._nombre = nombre;
+		this._telefono = telefono;
+	}
 
-function myFunction() {
-    var x = document.getElementById("display-name").value;
- //document.getElementById("demo").innerHTML = x;
- 	if(!isNaN(x)){
-    document.getElementById("nuevo").innerHTML = x;
-	console.log(x)}
-    else{
-    console.log("no esta bien");
-    }
+	get nombre(){
+		return this._nombre;
+	}
+
+	get telefono(){
+		return this._telefono;
+	}
+
+	validateName(){
+			if(/^[a-zA-Z\s]+$/.test(this._nombre)){
+				 document.getElementById("nuevo").innerHTML = this._nombre;
+				 alert("basue");
+				 console.log("no hace nada");
+				 return 1;
+					}
+				else{
+					alert("* el nombre tiene caracteres no validos.");
+					return 2;
+					}
+				}
+
+	validatePhone(){
+
+	}
+
 }
 
-function validateNumber() {
-	const numero = document.getElementById("nombre").value;
-	if(/^[a-zA-Z]+$/.test(numero))
+//principal
+
+function validate(){
+	x = document.getElementById("display-name").value;
+	console.log(x);
+	const newValidation = new veriStrings(x, 233456754);
+	console.log(newValidation.validateName());
+
 }
