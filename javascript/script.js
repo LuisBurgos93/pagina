169dff041aefbe797f829d0b8268c6c26612d671
LuisBurgos93 <burgos.luisfernando@gmail.com>
2018-09-25 12:05:33 -0500

    numero 12

--- javascript/script.js
+++ javascript/script.js
@@ -15,18 +15,19 @@ class veriStrings{
 	validateName(){
 			if(/^[a-zA-Z\s]+$/.test(this._nombre)){
 				 document.getElementById("nuevo").innerHTML = this._nombre;
-				 alert("basue");
 				 console.log("no hace nada");
-				 return 1;
 					}
 				else{
 					alert("* el nombre tiene caracteres no validos.");
-					return 2;
 					}
 				}
 
 	validatePhone(){
-
+			if(/^[0-9\s]+$/.test(this._telefono)){
+				document.getElementById("nuevo").innerHTML = this._numero;
+			}else{
+				alert("* el numero no es valido");
+			}
 	}
 
 }
@@ -35,8 +36,19 @@ class veriStrings{
 
 function validate(){
 	x = document.getElementById("display-name").value;
-	console.log(x);
-	const newValidation = new veriStrings(x, 233456754);
+	y = document.getElementById("disabled-ip").value;
+	console.log(x+"   "+y);
+	const newValidation = new veriStrings(x, y);
+	if((newValidation.nombre === "")&&(newValidation.telefono === "")){
+		alert("debes introducir al menos un campo");
+	}else if((newValidation.nombre != "")&&(newValidation.numero === "")) {
+		newValidation.validateName();
+	}else if((newValidation.nombre === "")&&(newValidation.numero != "")) {
+		newValidation.validatePhone();
+	}else{
+		newValidation.validateName();
+		newValidation.validatePhone();
+	}
 	console.log(newValidation.validateName());
 
 }
